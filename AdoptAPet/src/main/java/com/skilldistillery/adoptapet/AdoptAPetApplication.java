7eec51c97bd60b2c89cf550caf8b248200940134
Filipe Alves De Souza <filipewallace19@gmail.com>
2022-05-19 16:20:06 -0400

    create boot mvc project, add jpa project dependency

---
+++ AdoptAPet/src/main/java/com/skilldistillery/adoptapet/AdoptAPetApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.adoptapet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptAPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptAPetApplication.class, args);
+	}
+
+}
---
+++ AdoptAPet/src/test/java/com/skilldistillery/adoptapet/AdoptAPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.adoptapet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptAPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
