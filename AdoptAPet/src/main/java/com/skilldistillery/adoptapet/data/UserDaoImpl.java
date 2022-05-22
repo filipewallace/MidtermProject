d73cf0f7f34d450e27bc4c788ec200e2bb540ad5
Filipe Alves De Souza <filipewallace19@gmail.com>
2022-05-22 16:50:58 -0400

    no change

--- AdoptAPet/src/main/java/com/skilldistillery/adoptapet/data/UserDaoImpl.java
+++ AdoptAPet/src/main/java/com/skilldistillery/adoptapet/data/UserDaoImpl.java
@@ -1,5 +1,7 @@
 package com.skilldistillery.adoptapet.data;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -21,11 +23,27 @@ public class UserDaoImpl implements UserDAO {
 		return em.find(User.class, userId);
 	}
 
+//	@Override
+//	public User findByUsernameAndPassword(String username, String password) {
+//		
+//		return null;
+////		return em.find(User.class, username, password);
+//	}
+	
 	@Override
 	public User findByUsernameAndPassword(String username, String password) {
-		
-		return em.find(User.class, username, password);
+		User u = null;
+		String query2 = "SELECT u FROM User u";
+		List<User> users = em.createQuery(query2, User.class).getResultList();
+		for (User user : users) {
+			if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+				u = user;
+				break;
+			}
+		}
+		return u;
 	}
+	
 
 	@Override
 	public User createUser(User user) {
